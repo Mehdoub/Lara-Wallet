ab32a67f57df80b3dd2850590683e2dd6d72b79c
Mehdoub <mehdoub2020@gmail.com>
2023-10-30 17:11:09 +0330

    feat: - sentry package installed and configured

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -31,12 +31,13 @@ class Handler extends ExceptionHandler
     /**
      * Register the exception handling callbacks for the application.
      */
-    public function register(): void
-    {
+    public function register() {
         $this->reportable(function (Throwable $e) {
-            //
+          if (app()->bound('sentry')) {
+            app('sentry')->captureException($e);
+          }
         });
-    }
+      }
 
     public function render($request, Throwable $e)
     {
