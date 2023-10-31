1f93a78d3f2de6ad953a37383caf15bd471fd88c
Mehdoub <mehdoub2020@gmail.com>
2023-10-31 08:49:06 +0330

    feat: - delete deprecated payments after 24 houres command and job created

---
+++ app/Jobs/DeleteDeprecatedPaymentJob.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Payment;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteDeprecatedPaymentJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(private array $paymentIdList)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $deprecatedPayments = Payment::query()
+            ->whereIn('id', $this->paymentIdList)
+            ->get();
+        foreach ($deprecatedPayments as $deprecatedPayment) {
+            $deprecatedPayment->delete();
+        }
+    }
+}
--- config/settings.php
+++ config/settings.php
@@ -3,6 +3,8 @@
 return [
     'system' => [
         'api_request_limit_rate' => '60,1',
+        'delete_deprecated_payment_after_hours' => 24,
+        'delete_deprecated_payment_count' => 20,
     ],
     'global' => [
         'pagination' => 10,
